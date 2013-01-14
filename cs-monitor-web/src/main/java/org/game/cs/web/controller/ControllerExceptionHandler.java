c59b60e011c737bde09832d48adb8f18cbf92c55
Krisztian Horvath <keyki.kk@gmail.com>
2013-01-14 09:25:17 +0100

    exception handler refactor

--- cs-monitor-web/src/main/java/org/game/cs/web/controller/ControllerExceptionHandler.java
+++ cs-monitor-web/src/main/java/org/game/cs/web/controller/ControllerExceptionHandler.java
@@ -21,44 +21,48 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(value = {RequestTimeoutException.class, InterruptedException.class, SocketTimeoutException.class})
     public String handleExceptions1(Exception exception, HttpServletRequest request) {
-        request.setAttribute("error", "Could not connect to the server");
+        addErrorMessageToRequest(request, "Could not connect to the server");
         addServersToReqest(request);
         return "errors";
     }
 
     @ExceptionHandler(value = {NumberFormatException.class})
     public String handleExceptions2(Exception exception, HttpServletRequest request) {
-        request.setAttribute("error", "Wrong format");
+        addErrorMessageToRequest(request, "Wrong format");
         addServersToReqest(request);
         return "errors";
     }
 
     @ExceptionHandler(value = {IllegalArgumentException.class})
     public String handleExceptions3(Exception exception, HttpServletRequest request) {
-        request.setAttribute("error", "Wrong address");
+        addErrorMessageToRequest(request, "Wrong address");
         addServersToReqest(request);
         return "errors";
     }
 
     @ExceptionHandler(value = {NotConnectedException.class})
     public String handleExceptions4(Exception exception, HttpServletRequest request) {
-        request.setAttribute("error", exception.getMessage());
+        addErrorMessageToRequest(request, exception.getMessage());
         addServersToReqest(request);
         return "errors";
     }
 
     @ExceptionHandler(value = {FailedLoginException.class})
     public String handleExceptions5(Exception exception, HttpServletRequest request) {
-        request.setAttribute("error", "Wrong rcon password");
+        addErrorMessageToRequest(request, "Wrong rcon password");
         addServersToReqest(request);
         return "errors";
     }
 
     @ExceptionHandler(value = {ServerAlreadyRegisteredException.class})
     public String handleExceptions6(Exception exception, HttpServletRequest request) {
-        request.setAttribute("error", exception.getMessage());
+        addErrorMessageToRequest(request, exception.getMessage());
         addServersToReqest(request);
         return "errors";
+    }
+
+    private void addErrorMessageToRequest(HttpServletRequest request, String message) {
+        request.setAttribute("error", message);
     }
 
     private void addServersToReqest(HttpServletRequest request) {
