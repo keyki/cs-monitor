84029ce7d5b714fdf285bb1662fd673c54875a80
Krisztian Horvath <keyki.kk@gmail.com>
2013-01-21 16:49:22 +0100

    multipartResolver

--- cs-monitor-web/src/main/java/org/game/cs/web/init/WebMvcConfig.java
+++ cs-monitor-web/src/main/java/org/game/cs/web/init/WebMvcConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -38,6 +40,12 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         urlBasedViewResolver.setOrder(0);
         urlBasedViewResolver.setViewClass(TilesView.class);
         return urlBasedViewResolver;
+    }
+
+    @Bean(name = "multipartResolver")
+    public MultipartResolver getCommonsMultipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        return multipartResolver;
     }
 
 }
