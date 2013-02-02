e8afd547eaa95c2b9ac414a917b4efdde49de67f
Richard Doktorics <doktoricsrichard@gmail.com>
2013-02-02 12:27:33 +0100

    ethods with lovcal variables

--- cs-monitor-web/src/main/webapp/resources/js/home.js
+++ cs-monitor-web/src/main/webapp/resources/js/home.js
@@ -4,7 +4,6 @@ function() {
 
 	$('#cs_banner').parallax("50%", -0.3);
 	$('.dropdown-toggle').dropdownHover();
-	$(".center").center();
 	jQuery.fn.center = function () {
 	    this.css("position","absolute");
 	    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
@@ -13,11 +12,6 @@ function() {
 	                                                $(window).scrollLeft()) + "px");
 	    return this;
 	};
-	
-	
-	
-	
-	
 	
 	function Scroller(){
 		this.id="#conn-div";
@@ -31,46 +25,71 @@ function() {
 		this.offset="offset3";
 		this.pressedSize="70%";
 		this.originalSize="90%";
-	}
+		this.$lefty = $("#conn-div");
+	};
 	
 	Scroller.prototype.decider=function(){
-		
-	}
+		 
+		 if(parseInt(this.$lefty.css(this.modifyClass),10)<0){
+			 this.pullLeft();
+		 }else if(parseInt(this.$lefty.css(this.modifyClass),10)>0){
+			 this.pullRight();
+		 }
+		 
+	};
 	
 	Scroller.prototype.pullLeft=function(){
-		
-	}
+		 this.$lefty.animate({"left": parseInt( this.$lefty.css(this.modifyClass),10) == GetWidth()*0.03 ? -this.$lefty.outerWidth() : GetWidth()*0.03},"1000");
+		 $('#slideleft>i').removeClass('icon-chevron-right').addClass('icon-chevron-left');
+		 if(GetWidth()*0.2>240){
+		    	$('#connectionform').width('100%');
+		    	$('#content-div').width('70%');
+		    	$('#content-div').addClass('offset3');
+		 }else{
+		    	 $('#connectionform').width(240);
+		    	 $('#content-div').width('70%');
+		    	 $('#content-div').addClass('offset3');
+		 }
+	};
 	
 	Scroller.prototype.pullRight=function(){
-		
-	}
+		this.$lefty.animate({left: parseInt(this.$lefty.css(this.modifyClass),-10) == -GetWidth()*0.5 ? this.$lefty.outerWidth() :-GetWidth()*0.5});
+		$('#slideleft>i').removeClass('icon-chevron-left').addClass('icon-chevron-right');
+		$('#content-div').width('90%');
+		$('#content-div').removeClass('offset3');
+	};
 	
 	
 	
-	$('#slideleft').click(function() {
-		    var $lefty = $("#conn-div");
-		    if(parseInt($lefty.css('left'),10)<0){
-			    $lefty.animate({"left": parseInt($lefty.css('left'),10) == GetWidth()*0.03 ? -$lefty.outerWidth() : GetWidth()*0.03},"1000");
-			    $('#slideleft>i').removeClass('icon-chevron-right').addClass('icon-chevron-left');
-			   
-			    if(GetWidth()*0.2>240){
-			    	$('#connectionform').width('100%');
-			    	$('#content-div').width('70%');
-			    	$('#content-div').addClass('offset3');
-			    }else{
-			    	 $('#connectionform').width(240);
-			    	 $('#content-div').width('70%');
-			    	 $('#content-div').addClass('offset3');
-			    }
-		   }else if(parseInt($lefty.css('left'),10)>0){
-			    $lefty.animate({
-			      left: parseInt($lefty.css('left'),-10) == -GetWidth()*0.5 ? $lefty.outerWidth() :-GetWidth()*0.5
-			   });
-			   $('#slideleft>i').removeClass('icon-chevron-left').addClass('icon-chevron-right');
-			   $('#content-div').width('90%');
-			   $('#content-div').removeClass('offset3');
-		   }
-	 });
+	$('#slideleft').click(function(){
+		(new Scroller()).decider();
+	});
+			
+			
+//			{
+//		   
+//		    if(parseInt($lefty.css('left'),10)<0){
+//			    $lefty.animate({"left": parseInt($lefty.css('left'),10) == GetWidth()*0.03 ? -$lefty.outerWidth() : GetWidth()*0.03},"1000");
+//			    $('#slideleft>i').removeClass('icon-chevron-right').addClass('icon-chevron-left');
+//			   
+//			    if(GetWidth()*0.2>240){
+//			    	$('#connectionform').width('100%');
+//			    	$('#content-div').width('70%');
+//			    	$('#content-div').addClass('offset3');
+//			    }else{
+//			    	 $('#connectionform').width(240);
+//			    	 $('#content-div').width('70%');
+//			    	 $('#content-div').addClass('offset3');
+//			    }
+//		   }else if(parseInt($lefty.css('left'),10)>0){
+//			   $lefty.animate({
+//			      left: parseInt($lefty.css('left'),-10) == -GetWidth()*0.5 ? $lefty.outerWidth() :-GetWidth()*0.5
+//			   });
+//			   $('#slideleft>i').removeClass('icon-chevron-left').addClass('icon-chevron-right');
+//			   $('#content-div').width('90%');
+//			   $('#content-div').removeClass('offset3');
+//		   }
+//	 });
 	 
 	 
 	 
