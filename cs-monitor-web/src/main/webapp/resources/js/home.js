4c889d42f5c60e89c9da7014672ad37f54dd58db
Richard Doktorics <doktoricsrichard@gmail.com>
2013-02-02 13:17:56 +0100

    refactored methods

--- cs-monitor-web/src/main/webapp/resources/js/home.js
+++ cs-monitor-web/src/main/webapp/resources/js/home.js
@@ -29,66 +29,68 @@ function() {
 	};
 	
 	Scroller.prototype.decider=function(){
-		 
-		 if(parseInt(this.$lefty.css(this.modifyClass),10)<0){
-			 this.pullLeft();
-		 }else if(parseInt(this.$lefty.css(this.modifyClass),10)>0){
-			 this.pullRight();
-		 }
-		 
+		var $lefty = $(this.$lefty);	
+		if(parseInt($lefty.css(this.modifyClass),10)<0){
+				this.pullLeft();
+		}else if(parseInt($lefty.css(this.modifyClass),10)>0){
+			   this.pullRight();
+		}
 	};
 	
 	Scroller.prototype.pullLeft=function(){
-		 this.$lefty.animate({left: parseInt( this.$lefty.css(this.modifyClass),10) == GetWidth()*0.03 ? -this.$lefty.outerWidth() : GetWidth()*0.03},this.animateSpeed);
-		 $(this.iconSelector).removeClass(this.modifyIconRightClass).addClass(this.modifyIconLeftClass);
-		 if(GetWidth()*0.2>240){
-		    	$(this.formSelector).width('100%');
-		    	$(this.nextDiv).width(this.pressedSize);
-		    	$(this.nextDiv).addClass(this.offset);
-		 }else{
-		    	 $(this.formSelector).width(240);
-		    	 $(this.nextDiv).width(this.pressedSize);
-		    	 $(this.nextDiv).addClass(this.offset);
-		 }
+		var $lefty = $(this.id);	
+		$lefty.animate({left: parseInt($lefty.css(this.modifyClass),10) == GetWidth()*0.03 ? -$lefty.outerWidth() : GetWidth()*0.03},this.animateSpeed);
+	    $(this.iconSelector).removeClass(this.modifyIconRightClass).addClass(this.modifyIconLeftClass);
+	   
+	    if(GetWidth()*0.2>240){
+	    	$(this.formSelector).width('100%');
+	    	$(this.nextDiv).width(this.pressedSize);
+	    	$(this.nextDiv).addClass(this.offset);
+	    }else{
+	    	 $(this.formSelector).width(240);
+	    	 $(this.nextDiv).width(this.pressedSize);
+	    	 $(this.nextDiv).addClass(this.offset);
+	    }
 	};
 	
 	Scroller.prototype.pullRight=function(){
-		this.$lefty.animate({left: parseInt(this.$lefty.css(this.modifyClass),-10) == -GetWidth()*0.5 ? this.$lefty.outerWidth() :-GetWidth()*0.5},this.animateSpeed);
-		$(this.iconSelector).removeClass(this.modifyIconLeftClass).addClass(this.modifyIconRihtClass);
-		$(this.nextDiv).width(this.originalSize);
-		$(this.nextDiv).removeClass(this.offset);
+		var $lefty = $(this.id);	
+		$lefty.animate({left: parseInt($lefty.css(this.modifyClass),-10) == -GetWidth()*0.5 ? $lefty.outerWidth() :-GetWidth()*0.5 },this.animateSpeed);
+		   $(this.iconSelector).removeClass(this.modifyIconLeftClass).addClass(this.modifyIconRightClass);
+		   $(this.nextDiv).width(this.originalSize);
+		   $(this.nextDiv).removeClass(this.offset);
 	};
 	
 	var scroller=new Scroller();
 	$('#slideleft').click(function(){
 		scroller.decider();
-	});
+	 });
 			
-			
-//			{
-//		   
-//		    if(parseInt($lefty.css('left'),10)<0){
-//			    $lefty.animate({"left": parseInt($lefty.css('left'),10) == GetWidth()*0.03 ? -$lefty.outerWidth() : GetWidth()*0.03},"1000");
-//			    $('#slideleft>i').removeClass('icon-chevron-right').addClass('icon-chevron-left');
-//			   
-//			    if(GetWidth()*0.2>240){
-//			    	$('#connectionform').width('100%');
-//			    	$('#content-div').width('70%');
-//			    	$('#content-div').addClass('offset3');
-//			    }else{
-//			    	 $('#connectionform').width(240);
-//			    	 $('#content-div').width('70%');
-//			    	 $('#content-div').addClass('offset3');
-//			    }
-//		   }else if(parseInt($lefty.css('left'),10)>0){
-//			   $lefty.animate({
-//			      left: parseInt($lefty.css('left'),-10) == -GetWidth()*0.5 ? $lefty.outerWidth() :-GetWidth()*0.5
-//			   });
-//			   $('#slideleft>i').removeClass('icon-chevron-left').addClass('icon-chevron-right');
-//			   $('#content-div').width('90%');
-//			   $('#content-div').removeClass('offset3');
-//		   }
-//	 });
+	function decider(){
+		var $lefty = $("#conn-div");	
+		if(parseInt($lefty.css('left'),10)<0){
+			    $lefty.animate({left: parseInt($lefty.css('left'),10) == GetWidth()*0.03 ? -$lefty.outerWidth() : GetWidth()*0.03},"1000");
+			    $('#slideleft>i').removeClass('icon-chevron-right').addClass('icon-chevron-left');
+			   
+			    if(GetWidth()*0.2>240){
+			    	$('#connectionform').width('100%');
+			    	$('#content-div').width('70%');
+			    	$('#content-div').addClass('offset3');
+			    }else{
+			    	 $('#connectionform').width(240);
+			    	 $('#content-div').width('70%');
+			    	 $('#content-div').addClass('offset3');
+			    }
+		   }else if(parseInt($lefty.css('left'),10)>0){
+			   $lefty.animate({
+			      left: parseInt($lefty.css('left'),-10) == -GetWidth()*0.5 ? $lefty.outerWidth() :-GetWidth()*0.5
+			   });
+			   $('#slideleft>i').removeClass('icon-chevron-left').addClass('icon-chevron-right');
+			   $('#content-div').width('90%');
+			   $('#content-div').removeClass('offset3');
+		   }
+	}
+		   
 	 
 	 
 	 
