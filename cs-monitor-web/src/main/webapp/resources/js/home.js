b9e6930a425e8488402538aedca2cf48b9d2bfec
Richard Doktorics <doktoricsrichard@gmail.com>
2013-02-02 20:10:27 +0100

    it works finally

--- cs-monitor-web/src/main/webapp/resources/js/home.js
+++ cs-monitor-web/src/main/webapp/resources/js/home.js
@@ -12,7 +12,12 @@ function() {
 	                                                $(window).scrollLeft()) + "px");
 	    return this;
 	};
-	
+	  
+	var scroller=new Scroller();
+	$('#slideleft').click(function(){
+		scroller.decider();
+	});
+	  
 	function Scroller(){
 		this.id="#conn-div";
 		this.modifyClass="left";
@@ -24,79 +29,42 @@ function() {
 		this.formSelector='#connectionform';
 		this.offset="offset3";
 		this.pressedSize="70%";
-		this.originalSize="90%";
-		this.$lefty = $("#conn-div");
-	};
-	
-	Scroller.prototype.decider=function(){
-		var $lefty = $(this.$lefty);	
-		if(parseInt($lefty.css(this.modifyClass),10)<0){
-				this.pullLeft();
-		}else if(parseInt($lefty.css(this.modifyClass),10)>0){
-			   this.pullRight();
-		}
-	};
-	
-	Scroller.prototype.pullLeft=function(){
-		var $lefty = $(this.id);	
-		$lefty.animate({left: parseInt($lefty.css(this.modifyClass),10) == GetWidth()*0.03 ? -$lefty.outerWidth() : GetWidth()*0.03},this.animateSpeed);
-	    $(this.iconSelector).removeClass(this.modifyIconRightClass).addClass(this.modifyIconLeftClass);
-	   
-	    if(GetWidth()*0.2>240){
-	    	$(this.formSelector).width('100%');
-	    	$(this.nextDiv).width(this.pressedSize);
-	    	$(this.nextDiv).addClass(this.offset);
-	    }else{
-	    	 $(this.formSelector).width(240);
-	    	 $(this.nextDiv).width(this.pressedSize);
-	    	 $(this.nextDiv).addClass(this.offset);
-	    }
-	};
-	
-	Scroller.prototype.pullRight=function(){
-		var $lefty = $(this.id);	
-		$lefty.animate({left: parseInt($lefty.css(this.modifyClass),-10) == -GetWidth()*0.5 ? $lefty.outerWidth() :-GetWidth()*0.5 },this.animateSpeed);
-		   $(this.iconSelector).removeClass(this.modifyIconLeftClass).addClass(this.modifyIconRightClass);
-		   $(this.nextDiv).width(this.originalSize);
-		   $(this.nextDiv).removeClass(this.offset);
-	};
-	
-	var scroller=new Scroller();
-	$('#slideleft').click(function(){
-		scroller.decider();
-	 });
-			
-	function decider(){
-		var $lefty = $("#conn-div");	
-		if(parseInt($lefty.css('left'),10)<0){
-			    $lefty.animate({left: parseInt($lefty.css('left'),10) == GetWidth()*0.03 ? -$lefty.outerWidth() : GetWidth()*0.03},"1000");
-			    $('#slideleft>i').removeClass('icon-chevron-right').addClass('icon-chevron-left');
-			   
-			    if(GetWidth()*0.2>240){
-			    	$('#connectionform').width('100%');
-			    	$('#content-div').width('70%');
-			    	$('#content-div').addClass('offset3');
+		this.originalSize="90%";	
+	 };
+		
+	 Scroller.prototype.decider=function(){
+		 var $lefty = $(this.id);
+		 if(parseInt($lefty.css(this.modifyClass),10)<0){
+			 this.pullLeft();
+		 }else if(parseInt($lefty.css(this.modifyClass),10)>0){
+			 this.pullRight();
+		 }
+	 };
+		
+	 Scroller.prototype.pullLeft=function(){
+			var $lefty = $(this.id);	
+			 $lefty.animate({left: parseInt($lefty.css(this.modifyClass),10) == this.GetWidth()*0.03 ? -$lefty.outerWidth() : this.GetWidth()*0.03},this.animateSpeed);
+			    $(this.iconSelector).removeClass(this.modifyIconRightClass).addClass(this.modifyIconLeftClass);
+			    if(this.GetWidth()*0.2>240){
+			    	$(this.formSelector).width('100%');
 			    }else{
-			    	 $('#connectionform').width(240);
-			    	 $('#content-div').width('70%');
-			    	 $('#content-div').addClass('offset3');
+			    	 $(this.formSelector).width(240);
 			    }
-		   }else if(parseInt($lefty.css('left'),10)>0){
-			   $lefty.animate({
-			      left: parseInt($lefty.css('left'),-10) == -GetWidth()*0.5 ? $lefty.outerWidth() :-GetWidth()*0.5
-			   });
-			   $('#slideleft>i').removeClass('icon-chevron-left').addClass('icon-chevron-right');
-			   $('#content-div').width('90%');
-			   $('#content-div').removeClass('offset3');
-		   }
-	}
-		   
-	 
-	 
-	 
-	 function GetWidth(){
-		 return $(document).width();
-	  }
-	
+			    $(this.nextDiv).width(this.pressedSize);
+		    	$(this.nextDiv).addClass(this.offset);
+	 };
+		
+	 Scroller.prototype.GetWidth=function(){
+			 return $(document).width();
+	 };
+		
+	 Scroller.prototype.pullRight=function(){
+		   var $lefty = $(this.id);	
+		   $lefty.animate({
+			      left: parseInt($lefty.css(this.modifyClass),-10) == -this.GetWidth()*0.5 ? $lefty.outerWidth() :-this.GetWidth()*0.5 },this.animateSpeed);
+			   $(this.iconSelector).removeClass(this.modifyIconLeftClass).addClass(this.modifyIconRightClass);
+			   $(this.nextDiv).width(this.originalSize);
+			   $(this.nextDiv).removeClass(this.offset);
+	 };
 });
 
